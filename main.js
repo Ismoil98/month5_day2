9adf9577b153e128103b76436d901efe9001e76a
Ismoil98 <ismoil6366>
2024-01-31 18:11:30 +0500

    added

--- main.js
+++ main.js
@@ -32,7 +32,7 @@
 
 
 // CLASS-2 ///////////////
-// class Triangle {
+// class Employee {
 //     #leftSide;
 //     #rightSide;
 //     #width;
@@ -55,3 +55,130 @@
 // let res = new Triangle(3,4,5)
 // console.log(res);
 // console.log(Triangle.getLeftSide);
+
+
+
+// CLASS-3 ///////////////
+// class CustomDate {
+//     #leftSide;
+//     #rightSide;
+//     #width;
+//     constructor(leftSide, rightSide, width) {
+//       this.#leftSide = leftSide;
+//       this.#rightSide = rightSide;
+//       this.#width = width;
+//     }
+  
+//     getLeftSide() {
+//       return this.#leftSide;
+//     }
+//     getRightSide() {
+//       return this.#rightSide;
+//     }
+//     getWidth() {
+//       return this.#width;
+//     }
+//      getWidth() {
+//       return this.#width;
+//     }
+//      getWidth() {
+    //       return this.#width;
+    //     }
+//     setSides(a, b, c) {
+//       this.#leftSide = a;
+//       this.#rightSide = b;
+//       this.#width = c;
+//     }
+//   }
+ 
+// let res = new Triangle(3,4,5)
+// console.log(res);
+// console.log(Triangle.getLeftSide);
+
+
+
+// CLASS-4 ///////////////
+// class Time {
+//     #leftSide;
+//     #rightSide;
+//     #width;
+//     constructor(leftSide, rightSide, width) {
+//       this.#leftSide = leftSide;
+//       this.#rightSide = rightSide;
+//       this.#width = width;
+//     }
+  
+//     getLeftSide() {
+//       return this.#leftSide;
+//     }
+//     getRightSide() {
+//       return this.#rightSide;
+//     }
+//     getWidth() {
+//       return this.#width;
+//     }
+//     setSides(a, b, c) {
+//       this.#leftSide = a;
+//       this.#rightSide = b;
+//       this.#width = c;
+//     }
+//   }
+ 
+// let res = new Triangle(3,4,5)
+// console.log(res);
+// console.log(Triangle.getLeftSide);
+
+
+
+// CLASS-5 ///////////////
+// class Triangle {
+//     #leftSide;
+//     #rightSide;
+//     #width;
+//     constructor(leftSide, rightSide, width) {
+//       this.#leftSide = leftSide;
+//       this.#rightSide = rightSide;
+//       this.#width = width;
+//     }
+  
+//     getLeftSide() {
+//       return this.#leftSide;
+//     }
+//     getRightSide() {
+//       return this.#rightSide;
+//     }
+//     getWidth() {
+//       return this.#width;
+//     }
+//     setSides(a, b, c) {
+//       this.#leftSide = a;
+//       this.#rightSide = b;
+//       this.#width = c;
+//     }
+//   }
+ 
+// let res = new Triangle(3,4,5)
+// console.log(res);
+// console.log(Triangle.getLeftSide);
+
+
+// CLASS-7 ///////////////
+// class Animal {
+//   #radius = 0;
+//   constructor(radius) {
+//     this.#radius = radius;
+//   }
+//   get area() {
+//     return Math.PI * Math.pow(this.#radius, 2);
+//   }
+//   set radius(value) {
+//     if (typeof value === 'number' && value > 0) {
+//       this.#radius = value;
+//     } else {
+//       throw 'The radius must be a positive number';
+//     }
+//   }
+//   get radius() {
+//     return this.#radius;
+//   }
+// }
