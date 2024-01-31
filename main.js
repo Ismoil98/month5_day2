260054924190f62fc5414cd85924fd7a2a9660af
Ismoil98 <ismoil6366>
2024-01-31 18:07:53 +0500

    added

---
+++ main.js
@@ -0,0 +1,57 @@
+// CLASS-1 ///////////////
+// class Triangle {
+//     #leftSide;
+//     #rightSide;
+//     #width;
+//     constructor(leftSide, rightSide, width) {
+//       this.#leftSide = leftSide;
+//       this.#rightSide = rightSide;
+//       this.#width = width;
+//     }
+  
+//     getLeftSide() {
+//       return this.#leftSide;
+//     }
+//     getRightSide() {
+//       return this.#rightSide;
+//     }
+//     getWidth() {
+//       return this.#width;
+//     }
+//     setSides(a, b, c) {
+//       this.#leftSide = a;
+//       this.#rightSide = b;
+//       this.#width = c;
+//     }
+//   }
+ 
+// let res = new Triangle(3,4,5)
+// console.log(res);
+// console.log(Triangle.getLeftSide);
+
+
+
+// CLASS-2 ///////////////
+// class Triangle {
+//     #leftSide;
+//     #rightSide;
+//     #width;
+//     constructor(leftSide, rightSide, width) {
+//       this.#leftSide = leftSide;
+//       this.#rightSide = rightSide;
+//       this.#width = width;
+//     }
+  
+//     getLeftSide() {
+//       return this.#leftSide;
+//     }
+//     setSides(a, b, c) {
+//       this.#leftSide = a;
+//       this.#rightSide = b;
+//       this.#width = c;
+//     }
+//   }
+ 
+// let res = new Triangle(3,4,5)
+// console.log(res);
+// console.log(Triangle.getLeftSide);
